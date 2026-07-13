8dd518e0f091a6b70399759a1f5baa30c378fc6b
katsuro77 <itachisann109@gmail.com>
2026-07-13 17:23:24 +0300

    Fix port to 8000

--- app.py
+++ app.py
@@ -660,4 +660,4 @@ if __name__ == '__main__':
     else:
         print("🎭 Используется режим имитации (демо)")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
